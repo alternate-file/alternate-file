bb4c1f822046d9709080d4063a32f0d3729ae4a0
Will <will.ockelmann.wagner@gmail.com>
2019-10-12 02:04:21 -0700

    finish rollup config, don't bundle externals

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import pkg from "./package.json";
 import resolve from "rollup-plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
+import builtins from "builtin-modules";
 
 export default {
   input: "./src/index.ts",
@@ -16,11 +17,12 @@ export default {
       sourcemap: true
     }
   ],
+  external: [...builtins, "result-async", "find-up"],
   plugins: [
+    typescript(),
     resolve({
       extensions: ".ts",
       preferBuiltins: true
-    }),
-    typescript()
+    })
   ]
 };
